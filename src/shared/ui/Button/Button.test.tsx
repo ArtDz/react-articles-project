7c9cfff812ab5c6cc1625c930ac020762be7602f
ArtDz <dzugaev1998@bk.ru>
2023-06-27 21:31:44 +0300

    fix linter

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -3,12 +3,12 @@ import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 describe('Button', () => {
     test('test render', () => {
-        render(<Button>TEST</Button>)
-        expect(screen.getByText('TEST')).toBeInTheDocument()
-    })
+        render(<Button>TEST</Button>);
+        expect(screen.getByText('TEST')).toBeInTheDocument();
+    });
 
     test('test clear theme', () => {
-        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>)
-        expect(screen.getByText('TEST')).toHaveClass('clear')
-    })
-})
+        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+        expect(screen.getByText('TEST')).toHaveClass('clear');
+    });
+});
